959f6e528416814a68673cfea2745ed53e87c718
YonugJones <peterckerfoot@gmail.com>
2024-10-15 18:29:37 -0500

    added more styling added more initial data to populatedb.js changed title

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -3,7 +3,7 @@ const db = require('../db/queries');
 async function getRecords(req, res) {
   try {
     const records = await db.getRecordsQuery();
-    res.render('index', { title: 'My Record Collection', records });
+    res.render('index', { title: 'Stax of Wax', records });
   } catch (err) {
     console.error('Error fetching data:', err);
     res.status(500).send('Internal server error');
--- db/populatedb.js
+++ db/populatedb.js
@@ -15,11 +15,26 @@ CREATE TABLE IF NOT EXISTS records (
 
 INSERT INTO records (artist, album, genre, year, label) 
 VALUES
+  ('Sade', 'Promise', 'Funk/Soul, Pop', 1985, 'Portrait'),
+  ('Ahmad Jamal', 'Intervals', 'Jazz, Funk/Soul', 1980, '20th Century Fox Records'),
+  ('Missy Eliott', 'Supa Dupa Fly', 'Hip Hop', 1997, 'East West'),
+  ('Anita Baker', 'Rapture', 'Jazz, Funk/Soul', 1986, 'Elektra'),
+  ('Blu & Exile', 'Give Me My Flowers While I Can Still Smell Them', 'Hip Hop', 2012, 'Fat Beats'),
   ('Quasimoto', 'The Unseen', 'Hip Hop', 2000, 'Stones Throw'),
   ('Stevie Wonder', 'Innervisions', 'Soul', 1973, 'Tamla'),
   ('The Bill Evans Trio', 'Moon Beams', 'Jazz', 1962, 'Riverside Records'),
   ('The Headhunters', 'Survival of the Fittest', 'Funk', 1975, 'Arista'),
-  ('Chick Corea', 'Inner Space', 'Jazz', 1973, 'Atlantic');
+  ('Chick Corea', 'Inner Space', 'Jazz', 1973, 'Atlantic'),
+  ('Isaac Hayes', 'Hot Buttere Soul', 'Funk', 1969, 'Enterprise'),
+  ('John Coltrane', 'A Love Supreme: The Complete Masters', 'Jazz', 2016, 'Impulse!'),
+  ('Johnny McLaughlin', 'Electric Guitarist', 'Jazz', 1978, 'Columbia'),
+  ('Knxwledge', 'Hud Dreams', 'Hip Hop', 2015, 'Stones Throw'),
+  ('Marvin Gaye', 'I Want You', 'Funk/Soul', 1976, 'Tamla'),
+  ('Ohio Players', 'Fire', 'Funk/Soul', 1974, 'Mercury'),
+  ('Roy Ayers Ubiquity', 'Lifeline', 'Jazz, Funk/Soul', 1977, 'Polydor'),
+  ('Wayne Shorter', 'Juju', 'Jazz', 2014, 'Blue Note'),
+  ('Tuamie', 'Holy Ghost Spirituals', 'Hip Hop', 2017, 'Street Corner Music'),
+  ('U Roy', 'Version Galore', 'Reggae', 1971, 'Treasure Isle');
 `;
 
 async function main() {
