65b11678d58cf42a55582de818ea34c2c384d8c8
YonugJones <peterckerfoot@gmail.com>
2024-10-15 12:07:19 -0500

    added basic styling

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/new', newRouter);
 
