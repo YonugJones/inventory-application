8336dbb4b6053e636f324081c44005da123fcc2b
YonugJones <peterckerfoot@gmail.com>
2024-10-21 16:55:09 -0500

    fixed typo in connectionString

--- db/populatedb.js
+++ db/populatedb.js
@@ -41,7 +41,7 @@ async function main() {
   try {
     console.log('Seeding...');
     const client = new Client({
-      connectionString: process.env.DATABASE_URL
+      connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
     });
     await client.connect();
     await client.query(SQL);
